f4b67389398601aa1ee806ba775d03673f2cd931
Дмитрий <bykbyk80@mail.ru>
2024-08-27 21:33:22 +0500

    hw

--- homework1.py
+++ homework1.py
@@ -1,34 +1,18 @@
-# # Уровень 1
+# Уровень 1
 
-num1 = int(input('Введите первое целое число: '))
-num2 = int(input('Введите второе целое число: '))
-num3 = int(input('Введите третье целое число: '))
-
-if num1==num2==num3:
-    print(3)
-elif (num1!=num2 and num1==num3) or (num1!=num3 and num1==num2):
-    print(2)
-else:
-    print(0)
+value = (5+(7*5)/8)/3**5
+print('значение выражения:', value)
 
 # Уровень 2
 
-x1 = int(input('Введите номер по вертикали первой клетки (от 1 до 8): '))
-y1 = int(input('Введите номер по горизонтали первой клетки (от 1 до 8): '))
-x2 = int(input('Введите номер по вертикали второй клетки (от 1 до 8): '))
-y2 = int(input('Введите номер по горизонтали второй клетки (от 1 до 8): '))
-
-if x1==x2 or y1==y2:
-    print('YES')
-else:
-    print('NO')
+v = int(input('Введите скорость байкера: '))
+t = int(input('Введите время движения: '))
+point = (v*t)%109
+print(point)
 
 # Уровень 3
 
-password = input('Введите пароль: ')
-while True:
-    if len(password) > 8 and (password.lower() != password) and (password.upper() != password):
-        print('Пароль принят')
-        break
-
-    password = input('Введите пароль: ')
+num1 = float(input('Введите первое число: '))
+num2 = float(input('Введите второе число: '))
+max1 = (num1>num2)*num1+(num2>=num1)*num2
+print(max1)
